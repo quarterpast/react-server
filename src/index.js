90441e998130782ed2a82319ee25334bb77df813
Matt Brennan <mattyb1000@gmail.com>
2015-11-30 22:45:03 +0000

    mad hax to make the entry an actual thing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var watchify = require('watchify');
 var path = require('path');
 var from = require('from');
 var through = require('through2');
+var addStream = require('add-stream');
 
 exports.middleware = [
 	require('@quarterto/promise-server-react').withWrapHtml((html, title) => `<!doctype html>
@@ -20,30 +21,10 @@ exports.middleware = [
 ];
 
 function createBundle(resolved, options = {}) {
-	return browserify(Object.assign(options, {basedir: process.cwd()}, watchify.args))
-		.plugin(b => {
-			b.pipeline.get("record").push(through.obj(
-				function transform(row, enc, next) {
-					next(null, row);
-				},
-				function flush(next) {
-					const newEntryPath = path.resolve(process.cwd(), "___reactserver_entry.js")
-
-					this.push({
-						entry: true,
-						expose: false,
-						file: newEntryPath,
-						id: newEntryPath,
-						source: [
-							`require(${JSON.stringify(__dirname + '/client.js')}, entryId$$);`
-						],
-						nomap: true,
-						order: 0
-					})
-					next()
-				}
-			))
-		})
+	return browserify(resolved, Object.assign(options, {basedir: process.cwd()}, watchify.args))
+		.transform(file => file === resolved ? addStream(from([
+			`require(${JSON.stringify(__dirname + '/client.js')})(module.exports);`
+		])), through())
 		.transform('browserify-replace', {replace: [{from: /__ROUTES__/, to: `'${resolved}'`}]})
 		.transform('babelify');
 }
