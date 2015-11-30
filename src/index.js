3e114c1beded6aa49333c9ace1964c4e2b52bbd3
Matt Brennan <mattyb1000@gmail.com>
2015-11-30 22:21:12 +0000

    this seems increasingly ludicrous

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 var browserify = require('browserify');
 var watchify = require('watchify');
 var path = require('path');
-var from = require('from')
+var from = require('from');
+var through = require('through2');
 
 exports.middleware = [
 	require('@quarterto/promise-server-react').withWrapHtml((html, title) => `<!doctype html>
@@ -19,10 +20,30 @@ exports.middleware = [
 ];
 
 function createBundle(resolved, options = {}) {
-	return browserify(
-		from([`require('${__dirname + '/client.js'}')`]),
-		Object.assign(options, {basedir: process.cwd()}, watchify.args)
-	)
+	return browserify(Object.assign(options, {basedir: process.cwd()}, watchify.args))
+		.plugin(b => {
+			b.pipeline.get("record").push(through.obj(
+				function transform(row, enc, next) {
+					next(null, row);
+				},
+				function flush(next) {
+					const newEntryPath = resolve(process.cwd(), "___reactserver_entry.js")
+
+					this.push({
+						entry: true,
+						expose: false,
+						file: newEntryPath,
+						id: newEntryPath,
+						source: [
+							`require(${JSON.stringify("./" + origFilename)}, entryId$$);`
+						],
+						nomap: true,
+						order: 0
+					})
+					next()
+				}
+			))
+		})
 		.transform('browserify-replace', {replace: [{from: /__ROUTES__/, to: `'${resolved}'`}]})
 		.transform('babelify');
 }
